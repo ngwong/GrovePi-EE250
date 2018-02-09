1806b47aa19b7a2c0a0f0a9a8b1c6ef3a804ee95
Nilay Pachauri <pachauri@usc.edu>
2018-02-09 02:18:01 +0000

    Changes in the server code

--- ee250-lab04/part3/ledServer.py
+++ ee250-lab04/part3/ledServer.py
@@ -4,5 +4,48 @@ import sys
 sys.path.append('../../Software/Python/')
 
 from grovepi import *
+import time
+import socket
 
-# use TCP
+# Connect the Grove LED to digital port D2
+led = 2
+
+def Main():
+    host = '192.168.1.168'
+    port = 6000
+
+    s = socket.socket()
+    s.bind((host,port))
+
+    s.listen(1)
+    c, addr = s.accept()
+    print("Connection from: " + str(addr))
+    while True:
+        data = c.recv(1024).decode('utf-8')
+        if not data:
+            break
+        print("From connected user: " + data)
+        data = data.upper()
+
+        if (data == "LED_ON"): 
+            try:
+                digitalWrite(led,1)		# Send HIGH to switch on LED
+                data = "LED is now ON!"
+            except IOError:	# Print "Error" if communication error encountered
+                print ("Error")
+
+        elif (data == "LED_OFF"):
+            try:
+                digitalWrite(led,0)
+                data = "LED is now OFF"
+            except: IOError:
+                print("Error");
+        else:
+            data = "Error: Resend Message"
+
+        print("Sending: " + data)
+        c.send(data.encode('utf-8'))
+    c.close()
+
+if __name__ == '__main__':
+    Main()
