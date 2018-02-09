6b5415f1bc2a637da0ab39b65fffa5fe0bd69339
Nilay Pachauri <pachauri@usc.edu>
2018-02-09 00:47:53 +0000

    Ultrasonic Server code started

--- ee250-lab04/part3/ultrasonicServer.py
+++ ee250-lab04/part3/ultrasonicServer.py
@@ -4,5 +4,44 @@ import sys
 sys.path.append('../../Software/Python/')
 
 from grovepi import *
+import grovepi
+import socket
 
 #use UDP
+
+def Process1():
+    # Change the host and port as needed. For ports, use a number in the 9000 
+    # range. 
+    host = '10.0.2.15'
+    port = 9000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host,port))
+
+    print("Process 1 Server Started")
+    while True:
+        data, addr = s.recvfrom(1024)
+        data = data.decode('utf-8')
+        print("Message From: " + str(addr))
+        print("From connected user: " + data)
+        data = data.upper()
+        print("Sending: " + data)
+        s.sendto(data.encode('utf-8'), addr)
+    c.close()
+
+if __name__ == '__main__':
+    Process1()
+
+# Connect the Grove Ultrasonic Ranger to digital port D4
+# SIG,NC,VCC,GND
+ultrasonic_ranger = 3
+
+while True:
+    try:
+        # Read distance value from Ultrasonic
+        print(grovepi.ultrasonicRead(ultrasonic_ranger))
+
+    except TypeError:
+        print ("Error")
+    except IOError:
+        print ("Error")
