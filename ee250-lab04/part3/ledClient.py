7fb8cb9d8e130606e240d9d4f0e3f5310b113741
ngwong <ngwong@usc.edu>
2018-02-08 18:17:42 -0800

    ledClient.py code

--- ee250-lab04/part3/ledClient.py
+++ ee250-lab04/part3/ledClient.py
@@ -1,6 +1,35 @@
 import sys
+import socket
+#import time
 # By appending the folder of all the GrovePi libraries to the system path here,
 # we are successfully `from grovepi import *`
-sys.path.append('../../Software/Python/')
+#sys.path.append('../../Software/Python/')
 
-from grovepi import *
+#from grovepi import *
+
+
+def Main():
+    """127.0.0.1 is the loopback address. Any packets sent to this address will
+    essentially loop right back to your machine and look for any process 
+    listening in on the port specified."""
+    host = '10.0.2.15'
+    port = 5000
+    #led = 4
+
+    s = socket.socket() #by default, the socket constructor creates an TCP/IPv4 socket
+    s.connect((host,port))
+
+    message = input("-> ")
+    while message != 'q':
+        s.send(message.encode('utf-8')) 
+        #1024 is the receive buffer size. It's enough for us, and it's a nice number. 
+        data = s.recv(1024).decode('utf-8') 
+        print("Received from server: " + data)
+        message = input("-> ")
+    s.close()
+
+"""This if-statement checks if you are running this python file directly. That 
+is, if you run `python3 tcpClient.py` in terminal, this if-statement will be 
+true"""
+if __name__ == '__main__':
+    Main()
