314ca61f6ce6dffc634ee2c225aad255db239c13
Nilay Pachuari <pachauri@usc.edu>
2018-03-06 17:19:36 -0800

    Modfications to the MQTT functions

--- ee250/lab07/rpi_pub_and_sub.py
+++ ee250/lab07/rpi_pub_and_sub.py
@@ -5,6 +5,10 @@ Run rpi_pub_and_sub.py on your Raspberry Pi."""
 import paho.mqtt.client as mqtt
 import time
 from pynput import keyboard
+from grovepi import *
+
+led = 6
+ultra = 5
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
@@ -15,17 +19,40 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload))
 
-def on_press(key):
-if __name__ == '__main__':
-    #this section is covered in publisher_and_subscriber_example.py
-    client = mqtt.Client()
-    client.on_message = on_message
-    client.on_connect = on_connect
-    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
-    client.loop_start()
+#Custom callbacks need to be structured with three args like on_message()
+def custom_callback(client, userdata, message):
+    #the third argument is 'message' here unlike 'msg' in on_message 
+    if (str(message.payload) == "LED_ON"):
+        try:
+            digitalWrite(led, 1)
+        except IOError:
+            print ("Error")
+    elif (str(message.payload) == "LED_OFF"):
+        try:
+            digitalWrite(led, 0)
+        except IOError:
+            print ("Error")
 
-    while True:
-        print("delete this line")
-        time.sleep(1)
+    print("custom_callback: " + message.topic + " " + str(message.payload))
+    print("custom_callback: message.payload is of type " + 
+          str(type(message.payload)))
+
+
+def on_press(key):
+    if __name__ == '__main__':
+        #this section is covered in publisher_and_subscriber_example.py
+        client = mqtt.Client()
+        client.on_message = on_message
+        client.on_connect = on_connect
+        client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+        client.loop_start()
+
+        cleint.subscribe("anrg-pi10/led")
+        client.message_callback_add("anrg-pi10/led", custom_call_back)
+
+        while True:
+            
+            #print("delete this line")
+            time.sleep(1)
             
 
