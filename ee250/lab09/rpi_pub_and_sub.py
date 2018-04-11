eaf8b71aed537de343844bc3881117491c8f0e49
Nilay Pachuari <pachauri@usc.edu>
2018-04-10 17:56:28 -0700

    Converted all spaces to tabs

--- ee250/lab09/rpi_pub_and_sub.py
+++ ee250/lab09/rpi_pub_and_sub.py
@@ -15,59 +15,59 @@ humidity_temperature = 2
 led_status = 0
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected to server (i.e., broker) with result code "+str(rc))
+	print("Connected to server (i.e., broker) with result code "+str(rc))
 
-    # grovepi.pinMode(button, "INPUT")
+	# grovepi.pinMode(button, "INPUT")
 
-    client.subscribe("anrg-pi10/led")
-    client.message_callback_add("anrg-pi10/led", custom_callback_led)
+	client.subscribe("anrg-pi10/led")
+	client.message_callback_add("anrg-pi10/led", custom_callback_led)
 
-    client.subscribe("anrg-pi10/lcd")
-    client.message_callback_add("anrg-pi10/lcd", custom_callback_lcd)
-    #subscribe to topics of interest here
+	client.subscribe("anrg-pi10/lcd")
+	client.message_callback_add("anrg-pi10/lcd", custom_callback_lcd)
+	#subscribe to topics of interest here
 
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
-    print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
+	print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
 #Custom callbacks need to be structured with three args like on_message()
 def custom_callback_led(client, userdata, message):
-    #the third argument is 'message' here unlike 'msg' in on_message
-    global led_status # use the global variable so state change can be tracked
-    convMessage = str(message.payload, "utf-8") #converts massage payload from byte string to string
-    if ("LED_toggle" in convMessage): #checks payload if the LED needs to be toggled
-        try:
-        	led_status = 1 if led_status is 0 else 0
-        	digitalWrite(led, led_status)  #toggles LED
-        except IOError:
-            print ("Error")
+	#the third argument is 'message' here unlike 'msg' in on_message
+	global led_status # use the global variable so state change can be tracked
+	convMessage = str(message.payload, "utf-8") #converts massage payload from byte string to string
+	if ("LED_toggle" in convMessage): #checks payload if the LED needs to be toggled
+		try:
+			led_status = 1 if led_status is 0 else 0
+			digitalWrite(led, led_status)  #toggles LED
+		except IOError:
+			print ("Error")
 
-    print("custom_callback_led: " + message.topic + " " + convMessage)
-    print("custom_callback_led: message.payload is of type " + 
-          str(type(message.payload)))
+	print("custom_callback_led: " + message.topic + " " + convMessage)
+	print("custom_callback_led: message.payload is of type " + 
+		  str(type(message.payload)))
 
 def custom_callback_lcd(client, userdata, message):
-    convMessage2 = str(message.payload, "utf-8") #converts message payload from byte string to string
-    setText(convMessage2)#prints onto LED board
+	convMessage2 = str(message.payload, "utf-8") #converts message payload from byte string to string
+	setText(convMessage2)#prints onto LED board
 
 
 if __name__ == '__main__':
-    #this section is covered in publisher_and_subscriber_example.py
-    client = mqtt.Client()
-    client.on_message = on_message
-    client.on_connect = on_connect
-    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
-    client.loop_start()
+	#this section is covered in publisher_and_subscriber_example.py
+	client = mqtt.Client()
+	client.on_message = on_message
+	client.on_connect = on_connect
+	client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+	client.loop_start()
 
-    while True:  
+	while True:  
 
 
-        # Storing the humidity and temperature data
-        [humidity, temperature] = dht(humidity_temperature, 1)
+		# Storing the humidity and temperature data
+		[humidity, temperature] = dht(humidity_temperature, 1)
 
-        # Publishing the data to the MQTT server
+		# Publishing the data to the MQTT server
 		client.publish("anrg-pi10/temperature", temperature)
 		client.publish("anrg-pi10/humidity", humidity)
 
-        # client.publish("anrg-pi10/ultrasonicRanger", grovepi.ultrasonicRead(ultra))#publishes ultrasonic data
-        time.sleep(1) #timer so that ultrasonic only sends every second
+		# client.publish("anrg-pi10/ultrasonicRanger", grovepi.ultrasonicRead(ultra))#publishes ultrasonic data
+		time.sleep(1) #timer so that ultrasonic only sends every second
